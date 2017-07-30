bc8deb4d3a96523063ecd4960b2bf41ca247bb0f
Manas Mukherjee <mmanas19@gmail.com>
2017-07-30 12:48:23 -0700

    Panda POC

--- src/panda-poc/VectorizedMethods.py
+++ src/panda-poc/VectorizedMethods.py
@@ -44,7 +44,41 @@ def avg_bronze_medal_count():
     df = DataFrame(olympic_medal_counts)
 
     avg_bronze_at_least_one_gold =np.mean(df[df.gold>=1].bronze)
+    #print avg_bronze_at_least_one_gold
 
-    return avg_bronze_at_least_one_gold
+    # Avg Gold, Silver, Bronze in a new Series
+    # print np.mean(df['gold'])
+    print type(df[['gold','silver','bronze']].apply(np.mean))
 
-print avg_bronze_medal_count()
+if False:
+    avg_bronze_medal_count()
+
+
+def matrix_mul():
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea',
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+
+    olympic_medal_counts = {'country_name': Series(countries),
+                            'gold': Series(gold),
+                            'silver': Series(silver),
+                            'bronze': Series(bronze)}
+    df = DataFrame(olympic_medal_counts)
+
+    points = [4,3,1]
+    medal_count = df[['gold', 'silver', 'bronze']]
+    medal_count_points = np.dot(medal_count, points)
+
+    country_point_data = {'country_name': countries,'points': medal_count_points}
+    country_point_dataframe = DataFrame(country_point_data)
+    print country_point_dataframe
+
+if True:
+    matrix_mul()
